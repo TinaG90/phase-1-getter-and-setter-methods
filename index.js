4679a1560aa5778eb82c20751db6a6996330751d
Altenia Gaines <gaines.tina90@gmail.com>
2023-08-05 14:55:37 -0700

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    console.log(radius);
+    this.radius = radius;
+  }
+  get diameter() {
+    return this.radius * 2;
+  }
+  set diameter(newDiameter) {
+    this.radius = newDiameter / 2;
+  }
+  get circumference() {
+    return Math.PI * this.diameter;
+  }
+  set circumference(newCircumference) {
+    this.radius = newCircumference / 2 / Math.PI;
+  }
+  get area() {
+    return Math.PI * (this.radius * this.radius);
+  }
+  set area(newArea) {
+    this.radius = Math.sqrt(newArea / Math.PI);
+  }
+}
